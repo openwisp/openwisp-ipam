8d90d46ce290e2296a0a2ccf564d504a5c924f7a
Federico Capoano <nemesis@ninux.org>
2018-11-25 13:00:51 +0100

    [tests] Test openwisp-users integration

---
+++ openwisp_ipam/tests/test_users_integration.py
@@ -0,0 +1,11 @@
+from openwisp_users.tests.test_admin import TestUsersAdmin
+
+
+class TestUsersIntegration(TestUsersAdmin):
+    """
+    tests integration with openwisp_users
+    """
+    pass
+
+
+del TestUsersAdmin
