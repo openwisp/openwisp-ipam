27f36268abcac4ff7737bd79890e83d3c3f96e28
Ajay Tripathi <ajay39in@gmail.com>
2020-08-23 17:34:18 +0530

    [migration] Swapper migrations changes #48
    Closes #48

--- tests/openwisp2/sample_ipam/migrations/0002_remove_ipaddress_organization.py
+++ tests/openwisp2/sample_ipam/migrations/0002_remove_ipaddress_organization.py
@@ -10,5 +10,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(model_name='ipaddress', name='organization',),
+        migrations.RemoveField(model_name='ipaddress', name='organization'),
     ]
