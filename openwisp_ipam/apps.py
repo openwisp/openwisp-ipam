120a154d00a329ec70cae8f27130c1c4a67628dc
sankalp <sankalp123427@gmail.com>
2020-11-12 14:30:38 +0530

    [admin] Added menu items #37
    Added menu items (openwisp-utils menu) for Subnet and IpAdress models.
    
    Added test for menu items in test_admin.py
    
    Also updated openwisp-utils and openwisp-users dependency in requirements files.
    
    Fixes #37

--- openwisp_ipam/apps.py
+++ openwisp_ipam/apps.py
@@ -1,11 +1,18 @@
-from django.apps import AppConfig
+from openwisp_utils.api.apps import ApiAppConfig
+from openwisp_utils.utils import register_menu_items
+from swapper import get_model_name
 
 from .compat import patch_ipaddress_lib
 
 
-class OpenWispIpamConfig(AppConfig):
+class OpenWispIpamConfig(ApiAppConfig):
     name = 'openwisp_ipam'
     verbose_name = 'IPAM'
 
-    def ready(self):
+    def ready(self, *args, **kwargs):
         patch_ipaddress_lib()
+        items = [
+            {'model': get_model_name('openwisp_ipam', 'Subnet')},
+            {'model': get_model_name('openwisp_ipam', 'IpAddress')},
+        ]
+        register_menu_items(items, name_menu='OPENWISP_DEFAULT_ADMIN_MENU_ITEMS')
